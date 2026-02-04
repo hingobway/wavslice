7f98c22a87c9bb6c69cfdf33c7c24970fedc059a
hingobway <hingobway@users.noreply.github.com>
2026-02-04 00:25:48 -0500

    build changes
    - moved libsndfile to submodule
    - additional caching and logging in CI

--- src-py/build.sh
+++ src-py/build.sh
@@ -4,6 +4,11 @@ set -e
 
 source .venv/bin/activate
 
-nuitka --mode=onefile --show-progress --show-scons --python-flag=-u \
+nuitka \
+  --mode=onefile \
+  --show-progress \
+  --show-scons \
+  --nofollow-import-to=scipy.integrate._lebedev \
+  --python-flag=-u \
   --output-dir=out src/parse.py
 # --remove-output
