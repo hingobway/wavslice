f107d1bf7867d7bb7d0db2d4df6a5db81a2bbfc5
hingobway <hingobway@users.noreply.github.com>
2026-02-03 22:23:53 -0500

    workflow

--- src-py/build.sh
+++ src-py/build.sh
@@ -4,5 +4,5 @@ set -e
 
 source .venv/bin/activate
 
-nuitka --mode=onefile --output-dir=out src/parse.py
+nuitka --mode=onefile --quiet --show-progress --output-dir=out src/parse.py
 # --remove-output
--- src-py/build_setup.sh
+++ src-py/build_setup.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-python3 -m venv .venv
+python3.13 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
